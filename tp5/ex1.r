7a05ce1e022e7dddc9042dc8633d20782b973bfd
P-A <84812167+PA0Lbst@users.noreply.github.com>
2024-11-25 15:58:37 +0100

    Update ex1.r

--- tp5/ex1.r
+++ tp5/ex1.r
@@ -1,7 +1,3 @@
-
-# C'est moi ou ça affiche rien ?
-# HELP !
-
 U <- sample(1:5, size = 10000, replace = TRUE)
 V <- sample(1:5, size = 10000, replace = TRUE)
 
@@ -12,10 +8,6 @@ head(data.frame(U, V, X, Y))
 
 table(X, Y)
 
-# J'ai pas compris ça j'ai pompé sur Google :////////
-par(mfrow = c(1, 2))
-
+# Bon en fait pour voir les deux graphiques, il fallait juste cliquer sur la petite flèches, pas besoin de faire des sciences obscures.
 plot(table(X), main = "Distribution marginale de X", xlab = "X",  ylab = "Fréquence", type = "h", lwd = 2)
 plot(table(Y), main = "Distribution marginale de Y", xlab = "Y", ylab = "Fréquence", type = "h", lwd = 2)
-
-par(mfrow = c(1, 1))
