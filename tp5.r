64121a4e73e384bcbd629ecea055be4dfe4090b0
pa <padtroyes@gmail.com>
2024-11-25 11:44:54 +0100

    TP5 Ex2

--- tp5.r
+++ tp5.r
@@ -9,3 +9,13 @@ Y <- pmax(U, V)
 head(data.frame(U, V, X, Y))
 
 table(X, Y)
+
+# Exercice 2
+
+# J'ai pas compris ça j'ai pompé sur Google :////////
+par(mfrow = c(1, 2))
+
+plot(table(X), main = "Distribution marginale de X", xlab = "X",  ylab = "Fréquence", type = "h", lwd = 2)
+plot(table(Y), main = "Distribution marginale de Y", xlab = "Y", ylab = "Fréquence", type = "h", lwd = 2)
+
+par(mfrow = c(1, 1))
